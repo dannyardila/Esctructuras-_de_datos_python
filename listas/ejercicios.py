d11e3105535c8785d0c5fd9cbd07d5cda57a20f3
dannyardila <dannyardila059@gmail.com>
2022-10-21 07:21:05 -0500

    Se agregaron ejemplos de listas

--- listas/ejercicios.py
+++ listas/ejercicios.py
@@ -32,9 +32,38 @@ lista = lista + ["Calcetines"] + ["Bufanda"]
 print(lista)
 
 # 4. Modificar elementos de un a lista y borrar elementos de la misma
+print("---------Ejercicio 4---------")
 lista = ["Camiseta", "Pantalon", "Zapatillas"]
 print(lista)
 lista[1] ="Cazadora"
 print (lista)
 del lista[0]
 print(lista)
+
+# 5. Uso del operador *. Permite concatenar una lista con ella misma un numero finito de veces.
+print("---------Ejercicio 5---------")
+lista = ["Camiseta","Pantalon", "Zapatillas"]
+print(lista)
+lista_resultante = lista*3
+print(lista_resultante)
+
+#6. Creacion de listas como elementos de listas y acceso a dichos elementos.
+print("---------Ejercicio 6---------")
+lista = ["Camiseta", ["Calcetines", "Cazadores"],"Zapatillas"]
+print (lista)
+print (lista[0])
+print (lista[1])
+print (lista[2])
+print (lista[1][0])
+print (lista[1][1])
+
+# 7.Extraer una porcion d ela lista en una lusta nueva.
+print("----------Ejercicio 7-----------")
+lista = [1,2,3,4,5,6,7,8,9]
+print(lista)
+lista1 = lista[3:7]
+print(lista1)
+lista2 = lista[:5]
+print(lista2)
+lista3 = lista[6:]
+print(lista3)
---
+++ listas/ejercicios2.py
@@ -0,0 +1,37 @@
+#EJERCICIOS LISTAS
+
+# Metodos propios
+
+lista = [45,32,3,78]
+print("Lista original:", lista)
+# Funcion append: añade un elemento  a la lista
+lista.append(995)
+lista.append(7)
+print("Lista despues de usar appen:", lista)
+# Funcion sort: ordena la lista
+lista.sort
+print("Lista ordenada:", lista)
+# Funcion reverse: Invierte el orden de la lista
+lista.reverse()
+print("Lista al reves:",lista)
+# Funcion extend: Añade los elementos de una lista a lista
+lista_extend = [1,5,87,45]
+lista.extend(lista_extend)
+print("Lista despues de extend:",lista)
+# Funcion  count: cuenta el numero de veces que aparece el elemnto indicado como parametro dentro de la lista
+print("Numero de lementos 45:", lista.count(45))
+#funcion insert: Añade el elemento pasado como parametro a la lista en la posicion indicada tambien por el parametro
+lista.insert[4,111]
+print("lista despues de insert:",lista)
+# Funcion remove:elimin la primera ocurriencia empezando por la izquierda de la lista del elemento indicado por el parametro.
+lista.remove(45)
+print("Lista despues de remove:",lista)
+#Funcion index:devuelve la posicion de la  primera izquierda a derecha en la lista, del elemento pasado como parametro
+print("Posicion del elemento 111:", lista.index(111))
+# Funcion pop: Elimina un elemento de la lista y lo devuelve como resultado de la operacion.
+lista.pop()
+print("lista despues de pop: ", lista)
+# Funcion clear: elimina todos los elementos de la lista 
+lista.clear()
+print("Lista despues de clear: ", lista)
+
